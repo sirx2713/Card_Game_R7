8a4b1da17963e09e3ddbf4b915c5e6c88febb8d0
Mabende <belindamabende@gmail.com>
2024-05-30 07:19:37 +0200

    Initial Commit

---
+++ Card_Game_R7/Card_Game_R7App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R7App.swift
+//  Card_Game_R7
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/30.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
